9cb2ceaf8e11475949622765c26ad659d9033ba2
mrpeel <mrpeel@gmail.com>
2016-04-18 21:52:08 +1000

    Update service worker build

--- dist/sw.js
+++ dist/sw.js
@@ -1,6 +1,6 @@
 /*global self, caches, Promise, URL, location, fetch */
 
-var staticCacheName = 'smes-otg-v1460979436940';
+var staticCacheName = 'smes-otg-v1460980157884';
 var preCache = [
                 'material.min.css',
                 'material.min.js',
