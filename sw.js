dbddb4dd142d2285c4128dbe0a2038eb90cd9af8
mrpeel <mrpeel@gmail.com>
2018-03-08 21:25:50 +1100

    Update 2018-03-08T10:25:48.417Z

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 /*global self, caches, Promise, URL, location, fetch */
 
-var staticCacheName = 'smes-otg-v1520504155916';
+var staticCacheName = 'smes-otg-v1520504705007';
 var preCache = [
   'material.min.css',
   'material.min.js',
